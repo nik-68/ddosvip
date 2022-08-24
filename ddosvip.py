e1a5394dd5e909648a369ecff42babdd526b013b
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-24 10:53:50 +0300

    Update ddosvip.py

--- ddosvip.py
+++ ddosvip.py
@@ -12,8 +12,7 @@ import optparse
 import socket, requests
 import random
 import threading
-#----------------------------------------------------------------------#
-def ddos():
+
 print()
 print("""
     ██████╗░██████╗░░█████╗░░██████╗
@@ -23,11 +22,11 @@ print("""
     ██████╔╝██████╔╝╚█████╔╝██████╔╝
     ╚═════╝░╚═════╝░░╚════╝░╚═════╝ 
 """)
-    print ()
-    print ("MASUKAN IP DARI YANG ANDA INGIN SERANG")
-    print ()
-#----------------------------------------------------------------------#
-
+print ()
+print (" IP DARI YANG ANDA INGIN SERANG")
+print ()
+time.sleep(3)
+os.system("clear")
 
 useragents=["Mozilla/5.0 (Android; Linux armv7l; rv:10.0.1) Gecko/20100101 Firefox/10.0.1 Fennec/10.0.1","Mozilla/5.0 (Android; Linux armv7l; rv:2.0.1) Gecko/20100101 Firefox/4.0.1 Fennec/2.0.1","Mozilla/5.0 (WindowsCE 6.0; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
 "Mozilla/5.0 (Windows NT 5.1; rv:5.0) Gecko/20100101 Firefox/5.0",
