3583a0dd62b78537f325032b3ba8924f415741b0
nik-68 <106348996+nik-68@users.noreply.github.com>
2022-08-24 11:01:11 +0300

    Update ddosvip.py

--- ddosvip.py
+++ ddosvip.py
@@ -12,10 +12,18 @@ import optparse
 import socket, requests
 import random
 import threading
+#Colour
+yellow='\033[93m'
+gren='\033[92m'
+cyan='\033[96m'
+pink='\033[95m'
+red='\033[91m'
+b='\033[1m'
+##############
 
 os.system("clear")
 print()
-print("""
+print("""\033[93m
   ██████╗░██████╗░░█████╗░░██████╗
   ██╔══██╗██╔══██╗██╔══██╗██╔════╝
   ██║░░██║██║░░██║██║░░██║╚█████╗░
